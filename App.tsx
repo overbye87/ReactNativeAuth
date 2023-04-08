0f5d2bcb8bf849f469d023454ed7b79f1fddad34
overbye <overbye87@gmail.com>
2023-04-08 13:14:10 +0300

    first commit

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { ActivityIndicator, SafeAreaView, Text } from 'react-native';
+
+const App: React.FC = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello!</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
